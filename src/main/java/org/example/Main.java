76895f70a873867b271fae862238273a1a82d8c5
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-03-25 12:48:27 +0100

    subir2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+package org.example;
+
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         try (Connection connection = DriverManager.getConnection(
                 DBConfig.getUrl(),
                 DBConfig.getUser(),
                 DBConfig.getPassword()
-        )) {
-            System.out.println("Connected to database.");
+        );  Statement statement = connection.createStatement()) {
+
+            String sql = "SELECT * " +
+                    "FROM EMPLEADO " +
+                    "ORDER BY SALARIO DESC";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+               String nombre = resultSet.getString("NOMBRE");
+               double salario = resultSet.getDouble("SALARIO");
+
+               System.out.println("ID: "+id + " - Nombre: " + nombre + " ->> " + salario+ "€");
+            }
         } catch (SQLException e){
-            System.out.println("ERROR --> "+e.getMessage());
+            System.out.println("ERROR -> "+e.getMessage());
         }
     }
 }
